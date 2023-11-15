ee482cc5519b748d679df43b4b1b8581297a4683
Ssali Benjamin <benjamin.tamale@coseke.com>
2023-11-15 16:18:00 +0300

    Initialized repository

---
+++ src/pages/Home.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <React.Fragment>
+    <AppAppBar />
+    <ProductHero />
+    <ProductValues />
+    <ProductCategories />
+    <ProductHowItWorks />
+    <ProductCTA />
+    <ProductSmokingHero />
+    <AppFooter />
+  </React.Fragment>
+  )
+}
+
+export default Home;
